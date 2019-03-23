4303a61f4f07534433b0f9bd5f2cd2a10cc0e8fa
Ivan Novakov <ivan.novakov.dev@gmail.com>
2019-03-23 14:50:15 +0200

    Initialize project

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"context"
+	"github.com/gorilla/mux"
+	log "github.com/sirupsen/logrus"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/ping", func(writer http.ResponseWriter, request *http.Request) {
+		_, _ = writer.Write([]byte("pong"))
+	})
+	server := http.Server{
+		Addr:    ":8080",
+		Handler: r,
+	}
+	waitForClosingConnections := make(chan struct{})
+	go func() {
+		signalChan := make(chan os.Signal, 1)
+		signal.Notify(signalChan, os.Interrupt)
+		signal.Notify(signalChan, syscall.SIGTERM)
+
+		<-signalChan
+
+		log.Println("Shutting down server...")
+
+		if err := server.Shutdown(context.Background()); err!=nil {
+			log.Fatal(err)
+		}
+		close(waitForClosingConnections)
+
+	}()
+	if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		log.Fatal(err)
+	}
+
+	<-waitForClosingConnections
+}
