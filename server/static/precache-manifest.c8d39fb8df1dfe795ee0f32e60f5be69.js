79236af595d47d984e0166f202b02b22dc31f3a4
Ivan Novakov <ivan.novakov.dev@gmail.com>
2019-06-19 09:32:12 +0300

    Update web

---
+++ server/static/precache-manifest.c8d39fb8df1dfe795ee0f32e60f5be69.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cca8354b161fa7250db2780c4aecac68",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e10e15edf5ad3738e70e",
+    "url": "/static/js/2.54642b08.chunk.js"
+  },
+  {
+    "revision": "8c2d0ee56776439cba4a",
+    "url": "/static/js/main.92a06452.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
---
+++ server/static/static/js/main.92a06452.chunk.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{22:function(n,e,t){"use strict";t.r(e);var o=t(0),a=t.n(o),c=t(6),r=t.n(c),l=t(7),i=t.n(l);var p=function(){return a.a.createElement("div",{className:"App"},a.a.createElement(i.a,{value:'[flex]\nname = "My Flex Server"\nhost = "'.concat(window.location.hostname,'"\nport = ').concat(window.location.port)}))};r.a.render(a.a.createElement(p,null),document.getElementById("root"))},8:function(n,e,t){n.exports=t(22)}},[[8,1,2]]]);
+//# sourceMappingURL=main.92a06452.chunk.js.map
